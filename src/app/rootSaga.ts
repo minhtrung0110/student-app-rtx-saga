6ba91157f9cb58d7a14df44c00ebb0073ec15ad2
minhtrung0110 <nguyenducminhtrung0044@gmail.com>
2023-07-07 10:06:49 +0700

    chore(rootSaga): add StudentSaga and AuthSaga

--- src/app/rootSaga.ts
+++ src/app/rootSaga.ts
@@ -1,7 +1,9 @@
 import counterSaga from '../features/counter/couterSaga';
 import { all } from 'redux-saga/effects';
+import authSaga from 'src/features/auth/authSaga';
+import studentSaga from '../features/student/studentSaga';
 
 export default function* rootSaga() {
   console.log('Root Saga');
-  yield all([counterSaga()]);
+  yield all([counterSaga(), authSaga(), studentSaga()]);
 }
