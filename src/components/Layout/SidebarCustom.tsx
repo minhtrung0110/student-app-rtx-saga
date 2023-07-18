1a5d00677d1c80457c969b9983d6697d329ff346
minhtrung0110 <nguyenducminhtrung0044@gmail.com>
2023-07-18 17:51:12 +0700

    chore(UI) repair active Menu item

--- src/components/Layout/SidebarCustom.tsx
+++ src/components/Layout/SidebarCustom.tsx
@@ -3,6 +3,7 @@ import { Menu } from 'antd';
 import Sider from 'antd/es/layout/Sider';
 import styled from 'styled-components';
 import { THEME } from '../../constants';
+import { useLocation } from 'react-router-dom';
 
 interface SidebarProps {
   collapsed: boolean;
@@ -14,10 +15,18 @@ const SidebarStyled = styled(Sider)`
 `;
 
 const SidebarCustom: FC<SidebarProps> = ({ collapsed, listNavItems }) => {
+  const location = useLocation();
+  const currentPath = location.pathname;
   return (
     <SidebarStyled trigger={null} collapsible collapsed={collapsed} style={{ minHeight: 750 }}>
       <div className="demo-logo-vertical">Student-Project</div>
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} items={listNavItems} />
+      <Menu
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={['1']}
+        items={listNavItems}
+        selectedKeys={[currentPath]}
+      />
     </SidebarStyled>
   );
 };
